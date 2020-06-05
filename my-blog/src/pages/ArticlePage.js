700ee56bf7d99024b82e4c0469d69f38c8919df0
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-06-05 21:00:35 +0200

    site reading from database

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
@@ -7,6 +7,18 @@ const ArticlePage = ({ match }) => {
 	const name = match.params.name;
 	const article = articleContent.find(m => m.name === name);
 
+	const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await fetch(`/api/articles/${name}`);
+			const body = await result.json();
+			setArticleInfo(body);
+		};
+		fetchData();
+		//setArticleInfo({ upvotes: Math.ceil(Math.random() * 100) });
+	}, [name]);
+
 	if (!article) return <NotFoundPage />;
 
 	const otherArticles = articleContent.filter(m => m.name !== name);
@@ -14,6 +26,7 @@ const ArticlePage = ({ match }) => {
 	return (
 		<>
 			<h1>{article.title}</h1>
+			<p>This article has been upvoted {articleInfo.upvotes} times.</p>
 			{article.content.map((text, key) => (
 				<p key={key}>{text}</p>
 			))}
